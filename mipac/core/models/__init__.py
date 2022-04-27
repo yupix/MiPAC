759476d0994248fd45a8b45f15f94d4d0703a047
yupix <yupi0982@outlook.jp>
2022-04-27 17:09:20 +0900

    feat: __init__の定義

--- mipac/core/models/__init__.py
+++ mipac/core/models/__init__.py
@@ -1,1 +1,35 @@
-from .user import *
+from .chart import (
+    RawActiveUsersChart,
+    RawDriveChart,
+    RawDriveLocalChart,
+    RawDriveRemoteChart,
+)
+from .chat import RawChat
+from .drive import RawFile, RawFolder, RawProperties
+from .emoji import RawEmoji
+from .instance import RawInstance
+from .note import RawNote, RawReaction, RawRenote
+from .poll import RawPoll, RawPollChoices
+from .reaction import RawNoteReaction
+from .user import RawUser, RawUserDetails
+
+__all__ = (
+    'RawUserDetails',
+    'RawUser',
+    'RawActiveUsersChart',
+    'RawDriveRemoteChart',
+    'RawDriveLocalChart',
+    'RawDriveChart',
+    'RawNoteReaction',
+    'RawPoll',
+    'RawPollChoices',
+    'RawRenote',
+    'RawReaction',
+    'RawNote',
+    'RawInstance',
+    'RawEmoji',
+    'RawProperties',
+    'RawFolder',
+    'RawFile',
+    'RawChat',
+)
