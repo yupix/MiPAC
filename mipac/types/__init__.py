17a471b89e3387ad4fb27a4069c50f7459dc6f7e
yupix <yupi0982@outlook.jp>
2022-04-27 21:32:51 +0900

    chore __init__.py に __all__ を記述 issue #3

--- mipac/types/__init__.py
+++ mipac/types/__init__.py
@@ -1,9 +1,77 @@
-from .chart import *
-from .chat import *
-from .drive import *
-from .emoji import *
-from .instance import *
-from .note import *
-from .page import *
-from .reaction import *
-from .user import *
+from mipac.types.chart import (
+    ActiveUsersChartPayload,
+    DriveChartPayload,
+    DriveLocalChartPayload,
+    DriveRemoteChartPayload,
+)
+from mipac.types.chat import ChatPayload
+from mipac.types.drive import FilePayload, FolderPayload, PropertiesPayload
+from mipac.types.emoji import EmojiPayload
+from mipac.types.instance import (
+    FeaturesPayload,
+    InstancePayload,
+    MetaPayload,
+    OptionalInstance,
+    OptionalMeta,
+)
+from mipac.types.note import (
+    GeoPayload,
+    NotePayload,
+    OptionalReaction,
+    PollPayload,
+    ReactionPayload,
+    RenotePayload,
+)
+from mipac.types.page import (
+    AttachedFilePayload,
+    EyeCatchingImagePayload,
+    PageContentPayload,
+    PageFilePayload,
+    PagePayload,
+    VariablePayload,
+)
+from mipac.types.reaction import NoteReactionPayload
+from mipac.types.user import (
+    ChannelPayload,
+    FieldContentPayload,
+    OptionalUser,
+    PinnedNotePayload,
+    PinnedPagePayload,
+    UserPayload,
+)
+
+__all__ = (
+    'ActiveUsersChartPayload',
+    'DriveLocalChartPayload',
+    'DriveRemoteChartPayload',
+    'DriveChartPayload',
+    'ChatPayload',
+    'PropertiesPayload',
+    'FolderPayload',
+    'FilePayload',
+    'EmojiPayload',
+    'NotePayload',
+    'GeoPayload',
+    'ReactionPayload',
+    'PollPayload',
+    'RenotePayload',
+    'OptionalReaction',
+    'FeaturesPayload',
+    'MetaPayload',
+    'InstancePayload',
+    'OptionalInstance',
+    'OptionalMeta',
+    'PageContentPayload',
+    'VariablePayload',
+    'PageFilePayload',
+    'EyeCatchingImagePayload',
+    'AttachedFilePayload',
+    'PagePayload',
+    'NoteReactionPayload',
+    'ChannelPayload',
+    'FieldContentPayload',
+    'UserPayload',
+    'PinnedPagePayload',
+    'PinnedNotePayload',
+    'OptionalUser',
+)
