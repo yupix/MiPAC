b7bac42dc66346e2f1ae97c491c86a498c2c5f45
yupix <yupi0982@outlook.jp>
2022-05-13 07:32:05 +0900

    chore: 終わってるTODOを削除

--- mipac/manager/client.py
+++ mipac/manager/client.py
@@ -23,16 +23,16 @@ class ClientActions:
         self.user: UserManager = UserManager(session=session, client=self)
         self.admin: AdminActions = AdminActions(
             session=session, client=self
-        )  # TODO: 作る
+        )
         self.drive: DriveManager = DriveManager(
             session=session, client=self
-        )  # TODO: 作る
+        )
         self.reaction: ReactionManager = ReactionManager(
             session=session, client=self
-        )  # TODO: 作る
+        )
         self.chart: ChartManager = ChartManager(
             session=session, client=self
-        )  # TODO: 作る
+        )
 
     def _create_user_instance(self, user: User) -> UserManager:
         return UserManager(session=self.__session, client=self, user=user)
