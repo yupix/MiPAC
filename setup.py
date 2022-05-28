a96d643250713da56f9f2d7cdb7cd4458a34e3bc
yupix <yupi0982@outlook.jp>
2022-05-28 21:07:05 +0900

    chore: 開発環境に必要なライブラリを追記

--- setup.py
+++ setup.py
@@ -2,25 +2,17 @@ import pathlib
 
 from setuptools import setup
 
-description = "A Python wrapper for the Misskey API"
-readme_file = pathlib.Path(__file__).parent / "README.md"
-with readme_file.open(encoding="utf-8") as fh:
+description = 'A Python wrapper for the Misskey API'
+readme_file = pathlib.Path(__file__).parent / 'README.md'
+with readme_file.open(encoding='utf-8') as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as f:
+with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
 extras_require = {
-    'dev': [
-        'axblack',
-        'isort',
-        'mypy',
-        'pyproject-flake8'
-    ],
-    'ci': [
-        'pyproject-flake8',
-        'mypy'
-    ]
+    'dev': ['axblack', 'isort', 'mypy', 'pyproject-flake8', 'pre-commit'],
+    'ci': ['pyproject-flake8', 'mypy'],
 }
 
 packages = [
@@ -32,28 +24,27 @@ packages = [
     'mipac.manager',
     'mipac.manager.admin',
     'mipac.models',
-    'mipac.types'
-
+    'mipac.types',
 ]
 
 setup(
-    name="mipac",
-    version="0.0.2",
+    name='mipac',
+    version='0.0.2',
     install_requires=requirements,
-    url="https://github.com/yupix/mipac",
-    author="yupix",
-    author_email="yupi0982@outlook.jp",
-    license="MIT",
-    python_requires=">=3.10, <4.0",
+    url='https://github.com/yupix/mipac',
+    author='yupix',
+    author_email='yupi0982@outlook.jp',
+    license='MIT',
+    python_requires='>=3.10, <4.0',
     description=description,
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
     packages=packages,
     classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "Programming Language :: Python :: 3.10",
-        "Natural Language :: Japanese",
-        "License :: OSI Approved :: MIT License",
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.10',
+        'Natural Language :: Japanese',
+        'License :: OSI Approved :: MIT License',
     ],
     extras_require=extras_require,
 )
