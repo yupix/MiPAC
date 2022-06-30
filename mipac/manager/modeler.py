e1be082e19dcfe71eb5e03ec27d37811cc5029e7
yupix <yupi0982@outlook.jp>
2022-07-01 00:33:17 +0900

    feat: Modelerクラスを追加

---
+++ mipac/manager/modeler.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from mipac import File, User
+
+if TYPE_CHECKING:
+    from mipac.core import RawFile, RawUser
+    from mipac.manager.client import ClientActions
+
+
+class Modeler:
+    """
+    モデルを循環インポート無しでインスタンス化するためのクラスです
+    """
+
+    def __init__(self, client: ClientActions) -> None:
+        self._client = client
+
+    def create_user_instance(self, raw_user: RawUser) -> User:
+        return User(raw_user, client=self._client)
+
+    def create_file_instance(self, raw_file: RawFile) -> File:
+        return File(raw_file, client=self._client)
