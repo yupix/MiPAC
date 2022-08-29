bd44e2db81d0d10f22f91a020385a0f889f344a4
yupix <yupi0982@outlook.jp>
2022-08-30 01:25:13 +0900

    feat: InstanceLite モデルを追加

---
+++ mipac/models/lite/instance.py
@@ -0,0 +1,25 @@
+from mipac.types.instance import IInstanceLite
+
+
+class InstanceLite:
+    def __init__(self, instance: IInstanceLite) -> None:
+        self.__instance: IInstanceLite = instance
+
+    @property
+    def name(self) -> str:
+        return self.__instance['name']
+
+    def software_name(self) -> str:
+        return self.__instance['software_name']
+
+    def software_version(self) -> str:
+        return self.__instance['software_version']
+
+    def icon_url(self) -> str:
+        return self.__instance['icon_url']
+
+    def favicon_url(self) -> str:
+        return self.__instance['favicon_url']
+
+    def theme_color(self) -> str:
+        return self.__instance['theme_color']
