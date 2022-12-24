81a502e6fcac151dc01ee70a881fd0fab34a0b81
yupix <yupi0982@outlook.jp>
2022-12-24 16:42:32 +0000

    ✨ feat: added is_official attribute a Config class

--- mipac/config.py
+++ mipac/config.py
@@ -1,3 +1,6 @@
 class Config:
-    def __init__(self, is_ayuskey: bool = False) -> None:
+    def __init__(
+        self, is_ayuskey: bool = False, is_official: bool = False
+    ) -> None:
         self.is_ayuskey: bool = is_ayuskey
+        self.is_official: bool = is_official
