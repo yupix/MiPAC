0fb887acde9d9bf989875fa1a4f98f52af788f2e
omg-xtao <xtao@xtaolink.cn>
2022-12-24 20:37:39 +0800

    🎨 format: code

--- mipac/errors/base.py
+++ mipac/errors/base.py
@@ -4,6 +4,7 @@ from typing import Optional, Union
 
 class APIError(Exception):
     """APIのエラー"""
+
     def __init__(self, data: Union[dict, str], status: int):
         self.raw: Union[dict, str] = data
         self.status: int = status
@@ -11,18 +12,18 @@ class APIError(Exception):
         self.id: Optional[str] = None
         self.message: Optional[str] = None
         if isinstance(data, dict):
-            error = data.get("error", {})
-            self.code: Optional[str] = error.get("code", "")
-            self.id: Optional[str] = error.get("id")
-            self.message: Optional[str] = error.get("message", "")
+            error = data.get('error', {})
+            self.code: Optional[str] = error.get('code', '')
+            self.id: Optional[str] = error.get('id')
+            self.message: Optional[str] = error.get('message', '')
         super().__init__(self.message or self.raw)
 
     def raise_error(self):
         if not self.code:
             raise self
         if value := getattr(
-            import_module("mipac.errors.errors"),
-            "".join([i.capitalize() for i in self.code.split("_")]) + "Error",
+            import_module('mipac.errors.errors'),
+            ''.join([i.capitalize() for i in self.code.split('_')]) + 'Error',
             None,
         ):
             raise value(self.raw, self.status)
