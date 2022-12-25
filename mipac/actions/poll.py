3f11dc8b9ddc3125f464b84973299ca9a1abca5d
yupix <yupi0982@outlook.jp>
2022-12-25 10:06:18 +0000

    ✨ feat: added  method

--- mipac/actions/poll.py
+++ mipac/actions/poll.py
@@ -5,6 +5,8 @@ from typing import TYPE_CHECKING
 from mipac.abstract.action import AbstractAction
 from mipac.errors.base import ParameterError
 from mipac.http import HTTPClient, Route
+from mipac.models.note import Note
+from mipac.types.note import INote
 
 if TYPE_CHECKING:
     from mipac.manager.client import ClientActions
@@ -33,3 +35,40 @@ class PollActions(AbstractAction):
             Route('POST', '/api/notes/polls/vote'), auth=True, json=data
         )
         return res
+
+    async def recommendation(
+        self, limit: int = 100, offset: int = 0, all: bool = True
+    ):
+
+        if limit > 100:
+            raise ParameterError('limit must be less than 100')
+
+        if all:
+            limit = 100
+
+        async def request(body) -> list[Note]:
+            res: list[INote] = await self.__session.request(
+                Route('POST', '/api/notes/polls/recommendation'),
+                lower=True,
+                auth=True,
+                json=body,
+            )
+            return [Note(note, client=self.__client) for note in res]
+
+        data = {'limit': limit, 'offset': offset}
+        first_res: list[Note] = await request(data)
+        for note in first_res:
+            yield note
+        offset = limit
+        count = 1
+        if all and len(first_res) == limit:
+            data['offset'] = limit * count
+            while True:
+                res = await request(data)
+                if len(res) <= limit:
+                    for note in res:
+                        yield note
+                if len(res) == 0 or len(res) < limit:
+                    break
+                data['offset'] = limit * count
+                count = count + 1
