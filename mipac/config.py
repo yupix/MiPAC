6e3d414ee6b20e9f76ad4bd61d81a4cb155829c4
yupix <yupi0982@outlook.jp>
2023-01-17 08:50:45 +0000

    :sparkles: feat: configに属性hostとis_sslを追加

--- mipac/config.py
+++ mipac/config.py
@@ -17,20 +17,28 @@ class CacheConfig:
 class Config:
     def __init__(
         self,
+        host: str = '',
+        is_ssl: bool = True,
         is_ayuskey: bool = False,
         use_version: Literal[13, 12, 11] = 12,
         cache: CacheConfigData | None = None,
     ) -> None:
+        self.is_ssl: bool = is_ssl
+        self.host: str = host
         self.is_ayuskey: bool = is_ayuskey
         self.use_version: Literal[13, 12, 11] = use_version
         self.cache: CacheConfig = CacheConfig(cache or CacheConfigData())
 
     def from_dict(
         self,
+        host: str | None = None,
+        is_ssl: bool | None = None,
         is_ayuskey: bool | None = None,
         use_version: Literal[13, 12, 11] | None = None,
         cache: CacheConfigData | None = None,
     ):
+        self.host = host or self.host
+        self.is_ssl = is_ssl if is_ssl is not None else self.is_ssl
         self.is_ayuskey = is_ayuskey or self.is_ayuskey
         self.use_version = use_version or self.use_version
         if cache:
