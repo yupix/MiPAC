701ce780d8688c65e3b721f47253c211b3eae9cc
yupix <yupi0982@outlook.jp>
2023-01-17 07:06:27 +0000

    :sparkles: feat: `FederationInstance` クラスを追加

--- mipac/models/instance.py
+++ mipac/models/instance.py
@@ -3,12 +3,116 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from mipac.models.lite.instance import LiteInstanceMeta
-from mipac.types.instance import IInstanceMeta
+from mipac.types.instance import IFederationInstance, IInstanceMeta
 
 if TYPE_CHECKING:
     from mipac.client import ClientActions
 
 __all__ = ('InstanceMeta',)
+
+
+class FederationInstance:
+    def __init__(
+        self, instance: IFederationInstance, *, client: ClientActions
+    ) -> None:
+        self.__instance: IFederationInstance = instance
+        self.__client: ClientActions = client
+
+    @property
+    def id(self) -> str:
+        return self.__instance['id']
+
+    @property
+    def host(self) -> str:
+        return self.__instance['host']
+
+    @property
+    def users_count(self) -> int:
+        return self.__instance['users_count']
+
+    @property
+    def notes_count(self) -> int:
+        return self.__instance['notes_count']
+
+    @property
+    def following_count(self) -> int:
+        return self.__instance['following_count']
+
+    @property
+    def followers_count(self) -> int:
+        return self.__instance['followers_count']
+
+    @property
+    def is_not_responding(self) -> bool:
+        return self.__instance['is_not_responding']
+
+    @property
+    def is_suspended(self) -> bool:
+        return self.__instance['is_suspended']
+
+    @property
+    def is_blocked(self) -> bool:
+        return self.__instance['is_blocked']
+
+    @property
+    def software_name(self) -> str:
+        return self.__instance['software_name']
+
+    @property
+    def software_version(self) -> str:
+        return self.__instance['software_version']
+
+    @property
+    def open_registrations(self) -> bool:
+        return self.__instance['open_registrations']
+
+    @property
+    def name(self) -> str:
+        return self.__instance['name']
+
+    @property
+    def description(self) -> str:
+        return self.__instance['description']
+
+    @property
+    def maintainer_name(self) -> str:
+        return self.__instance['maintainer_name']
+
+    @property
+    def maintainer_email(self) -> str:
+        return self.__instance['maintainer_email']
+
+    @property
+    def icon_url(self) -> str:
+        return self.__instance['icon_url']
+
+    @property
+    def favicon_url(self) -> str:
+        return self.__instance['favicon_url']
+
+    @property
+    def theme_color(self) -> str:
+        return self.__instance['theme_color']
+
+    @property
+    def info_updated_at(self) -> str:
+        return self.__instance['info_updated_at']
+
+    @property
+    def caught_at(self) -> str | None:
+        return self.__instance.get('caught_at')
+
+    @property
+    def first_retrieved_at(self) -> str | None:
+        return self.__instance.get('first_retrieved_at')
+
+    @property
+    def latest_request_sent_at(self) -> str | None:
+        return self.__instance.get('latest_request_sent_at')
+
+    @property
+    def last_communicated_at(self) -> str | None:
+        return self.__instance.get('last_communicated_at')
 
 
 class InstanceMeta(LiteInstanceMeta):
--- mipac/types/instance.py
+++ mipac/types/instance.py
@@ -19,6 +19,36 @@ __all__ = (
 )
 
 
+class IFederationInstanceRequired(TypedDict):
+    id: str
+    host: str
+    users_count: int
+    notes_count: int
+    following_count: int
+    followers_count: int
+    is_not_responding: bool
+    is_suspended: bool
+    is_blocked: bool
+    software_name: str
+    software_version: str
+    open_registrations: bool
+    name: str
+    description: str
+    maintainer_name: str
+    maintainer_email: str
+    icon_url: str
+    favicon_url: str
+    theme_color: str
+    info_updated_at: str
+
+
+class IFederationInstance(IFederationInstanceRequired, total=False):
+    caught_at: str
+    first_retrieved_at: str
+    latest_request_sent_at: str
+    last_communicated_at: str
+
+
 class IInstanceLite(TypedDict):
     name: str
     software_name: str
