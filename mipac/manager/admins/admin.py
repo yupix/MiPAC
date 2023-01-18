9b31614d21cfcd94387c1140a9d554bd412c26f7
yupix <yupi0982@outlook.jp>
2023-01-18 12:08:57 +0000

    :sparkles: feat: AdminAnnouncementManagerクラスを追加 #38

--- mipac/manager/admins/admin.py
+++ mipac/manager/admins/admin.py
@@ -6,6 +6,7 @@ from mipac.abstract.manager import AbstractManager
 from mipac.actions.admins.admin import AdminActions
 from mipac.http import HTTPClient
 from mipac.manager.ad import AdminAdvertisingManager
+from mipac.manager.admins.announcement import AdminAnnouncementManager
 from mipac.manager.admins.emoji import AdminEmojiManager
 from mipac.manager.admins.moderator import AdminModeratorManager
 from mipac.manager.admins.user import AdminUserManager
@@ -18,10 +19,21 @@ class AdminManager(AbstractManager):
     def __init__(self, session: HTTPClient, client: ClientManager):
         self.__session: HTTPClient = session
         self.__client: ClientManager = client
-        self.emoji = AdminEmojiManager(session=session, client=client)
-        self.user = AdminUserManager(session=session, client=client)
-        self.ad = AdminAdvertisingManager(session=session, client=client)
-        self.moderator = AdminModeratorManager(session=session, client=client)
+        self.emoji: AdminEmojiManager = AdminEmojiManager(
+            session=session, client=client
+        )
+        self.user: AdminUserManager = AdminUserManager(
+            session=session, client=client
+        )
+        self.ad: AdminAdvertisingManager = AdminAdvertisingManager(
+            session=session, client=client
+        )
+        self.moderator: AdminModeratorManager = AdminModeratorManager(
+            session=session, client=client
+        )
+        self.announcement: AdminAnnouncementManager = AdminAnnouncementManager(
+            session=session, client=client
+        )
 
     @property
     def action(self) -> AdminActions:
