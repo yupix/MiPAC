cfdff90a26ef1c0aa3b53c567e8481f917234b2a
yupix <yupi0982@outlook.jp>
2023-01-19 10:59:50 +0000

    :sparkles: feat: server-info周りのモデルや型を追加 #41

--- mipac/models/admin.py
+++ mipac/models/admin.py
@@ -4,7 +4,14 @@ from datetime import datetime
 from typing import TYPE_CHECKING
 
 from mipac.models.user import UserDetailed
-from mipac.types.admin import IModerationLog
+from mipac.types.admin import (
+    IModerationLog,
+    IServerInfo,
+    IServerInfoCpu,
+    IServerInfoFs,
+    IServerInfoMem,
+    IServerInfoNet,
+)
 from mipac.util import str_to_datetime
 
 if TYPE_CHECKING:
@@ -39,3 +46,84 @@ class ModerationLog:
     @property
     def user(self) -> UserDetailed:
         return UserDetailed(self.__moderation_log['user'], client=self.__client)
+
+
+class ServerInfoCpu:
+    def __init__(self, server_info_cpu: IServerInfoCpu) -> None:
+        self.__server_info_cpu = server_info_cpu
+
+    @property
+    def models(self) -> str:
+        return self.__server_info_cpu['models']
+
+    @property
+    def cores(self) -> int:
+        return self.__server_info_cpu['cores']
+
+
+class ServerInfoMem:
+    def __init__(self, server_info_mem: IServerInfoMem) -> None:
+        self.__server_info_mem = server_info_mem
+
+    @property
+    def total(self) -> int:
+        return self.__server_info_mem['total']
+
+
+class ServerInfoFs:
+    def __init__(self, server_info_fs: IServerInfoFs) -> None:
+        self.__server_info_fs = server_info_fs
+
+    @property
+    def total(self) -> int:
+        return self.__server_info_fs['total']
+
+    @property
+    def used(self) -> int:
+        return self.__server_info_fs['used']
+
+
+class ServerInfoNet:
+    def __init__(self, server_info_net: IServerInfoNet) -> None:
+        self.__server_info_net = server_info_net
+
+    @property
+    def interface(self) -> str:
+        return self.__server_info_net['interface']
+
+
+class ServerInfo:
+    def __init__(self, server_info: IServerInfo) -> None:
+        self.__server_info = server_info
+
+    @property
+    def machine(self) -> str:
+        return self.__server_info['machine']
+
+    @property
+    def os(self) -> str:
+        return self.__server_info['os']
+
+    @property
+    def node(self) -> str:
+        return self.__server_info['node']
+
+    @property
+    def psql(self) -> str:
+        return self.__server_info['psql']
+
+    @property
+    def cpu(self) -> ServerInfoCpu:
+        return ServerInfoCpu(self.__server_info['cpu'])
+
+    @property
+    def mem(self) -> ServerInfoMem:
+        return ServerInfoMem(self.__server_info['mem'])
+
+    @property
+    def fs(self) -> ServerInfoFs:
+        return ServerInfoFs(self.__server_info['fs'])
+
+    @property
+    def net(self) -> ServerInfoNet:
+        return ServerInfoNet(self.__server_info['net'])
--- mipac/types/admin.py
+++ mipac/types/admin.py
@@ -13,3 +13,32 @@ class IModerationLog(TypedDict):
     info: dict  # TODO: これ何?
     user_id: str
     user: IUserDetailed
+
+
+class IServerInfoCpu(TypedDict):
+    models: str
+    cores: int
+
+
+class IServerInfoMem(TypedDict):
+    total: int
+
+
+class IServerInfoFs(TypedDict):
+    total: int
+    used: int
+
+
+class IServerInfoNet(TypedDict):
+    interface: str
+
+
+class IServerInfo(TypedDict):
+    machine: str
+    os: str
+    node: str
+    psql: str
+    cpu: IServerInfoCpu
+    mem: IServerInfoMem
+    fs: IServerInfoFs
+    net: IServerInfoNet
