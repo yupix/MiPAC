ac20be653fa0d885d1f69e5e3c7aca84fecd0a86
yupix <yupi0982@outlook.jp>
2023-01-19 09:28:51 +0000

    :sparkles: feat: update-metaをサポート #41

--- mipac/actions/admins/admin.py
+++ mipac/actions/admins/admin.py
@@ -6,8 +6,9 @@ from mipac.abstract.action import AbstractAction
 from mipac.errors.base import NotSupportVersion
 from mipac.http import HTTPClient, Route
 from mipac.models.meta import AdminMeta
-from mipac.types.meta import IAdminMeta
+from mipac.types.meta import IAdminMeta, IUpdateMetaBody
 from mipac.config import config
+from mipac.util import convert_dict_keys_to_camel
 
 if TYPE_CHECKING:
     from mipac.manager.client import ClientManager
@@ -40,5 +41,20 @@ class AdminActions(AbstractAction):
         return bool(
             await self.__session.request(
                 Route('POST', '/api/admin/update-user-note'), auth=True, json=body
+            )
+        )
+
+    async def update_meta(self, meta: IUpdateMetaBody) -> bool:
+        body = convert_dict_keys_to_camel(
+            meta, replace_list={'tos_text_url': 'ToSTextUrl', 'tos_url': 'ToSUrl'}
+        )
+
+        return bool(
+            await self.__session.request(
+                Route('POST', '/api/admin/update-meta'),
+                json=body,
+                auth=True,
+                lower=True,
+                remove_none=False,
             )
         )
