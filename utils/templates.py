be554de2b41b92799944d2ec19cf1bb9fbec1340
yupix <yupi0982@outlook.jp>
2023-03-31 18:28:34 +0900

    feat: mipactlを用いた際に自動でmanager側にactionsをインポートするように

--- utils/templates.py
+++ utils/templates.py
@@ -1,25 +1,28 @@
-MANAGER_TEMPLATE = """
+MANAGER_CLASS_NAME_TEMPLATE = '{0}Manager'
+MANAGER_TEMPLATE = '''
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from mipac.abstract.manager import AbstractManager
 from mipac.http import HTTPClient
+from {1} import {2}
 
 if TYPE_CHECKING:
     from mipac.manager.client import ClientManager
 
 
-class {0}Manager(AbstractManager):
+class {0}(AbstractManager):
     def __init__(self, *, session: HTTPClient, client: ClientManager):
         self.__session: HTTPClient = session
         self.__client: ClientManager = client
 
     @property
-    def action(self):
-        return\n
-"""
+    def action(self) -> {2}:
+        return {2}(session=self.__session, client=self.__client)
+'''
 
-ACTIONS_TEMPLATE = """
+ACTIONS_CLASS_NAME_TEMPLATE = '{0}Actions'
+ACTIONS_TEMPLATE = '''
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
@@ -31,10 +34,9 @@ if TYPE_CHECKING:
     from mipac.client import ClientManager
 
 
-class {0}Actions(AbstractAction):
+class {0}(AbstractAction):
     def __init__(self, *, session: HTTPClient, client: ClientManager):
         self.__session: HTTPClient = session
         self.__client: ClientManager = client\n
-"""
+'''
 
-TEMPLATES = {'manager': MANAGER_TEMPLATE, 'actions': ACTIONS_TEMPLATE}
