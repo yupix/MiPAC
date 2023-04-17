70beddbf2cff4333e12dbc647d8a2a1a7df8df5b
yupix <yupi0982@outlook.jp>
2023-04-17 08:31:02 +0000

    feat: Clientをインスタンス化する際にサーバーのバージョンを指定できるように

--- mipac/client.py
+++ mipac/client.py
@@ -1,4 +1,4 @@
-from mipac.config import Config, config
+from mipac.config import Config, IMisskeyVersions, config
 from mipac.http import HTTPClient
 from mipac.manager.client import ClientManager
 from mipac.utils.log import LOGING_LEVEL_TYPE, setup_logging
@@ -6,11 +6,18 @@ from mipac.utils.log import LOGING_LEVEL_TYPE, setup_logging
 
 class Client:
     def __init__(
-        self, url: str, token: str | None = None, *, log_level: LOGING_LEVEL_TYPE = 'INFO'
+        self,
+        url: str,
+        token: str | None = None,
+        *,
+        log_level: LOGING_LEVEL_TYPE = 'INFO',
+        use_version: IMisskeyVersions = 12,
+        use_version_autodetect: bool = True
     ) -> None:
         setup_logging(level=log_level)
         self.__url: str = url
         self.__token: str | None = token
+        config.from_dict(use_version=use_version, use_version_autodetect=use_version_autodetect)
         self.config: Config = config
         self.http: HTTPClient = HTTPClient(url, token)
 
