bf69165b3b94bf02d149e0776b57ddefd1559d71
yupix <yupi0982@outlook.jp>
2023-11-22 09:27:17 +0900

    chore: IPartialRoleに含まれる属性をIRoleから削除し、継承するように

--- mipac/types/roles.py
+++ mipac/types/roles.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, NotRequired, TypedDict, TypeGuard
+from typing import TYPE_CHECKING, Literal, NotRequired, TypedDict, TypeGuard
 
 if TYPE_CHECKING:
     from mipac.types.user import IMeDetailed, IUserDetailed
@@ -43,25 +43,6 @@ class IRolePolicies(TypedDict):
     rate_limit_factor: IRolePolicieValue
 
 
-class IRole(TypedDict):
-    id: str
-    created_at: str
-    updated_at: str
-    name: str
-    description: str
-    color: str | None
-    icon_url: str | None
-    target: str
-    cond_formula: dict
-    is_public: bool
-    is_administrator: bool
-    is_moderator: bool
-    as_badge: bool
-    can_edit_members_by_moderator: bool
-    policies: IRolePolicies
-    users_count: int
-
-
 class IPartialRole(TypedDict):
     id: str
     name: str
@@ -71,6 +52,19 @@ class IPartialRole(TypedDict):
     is_moderator: bool
     is_administrator: bool
     display_order: int
+
+
+class IRole(IPartialRole):
+    created_at: str
+    updated_at: str
+    target: Literal["manual", "conditional"]
+    cond_formula: dict
+    is_public: bool
+    is_explorable: bool
+    as_badge: bool
+    can_edit_members_by_moderator: bool
+    policies: IRolePolicies
+    users_count: int
 
 
 def is_me_role(data: IMeRole | IRoleUser, me_id: str) -> TypeGuard[IMeRole]:
