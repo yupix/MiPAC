5ad026dca623fcc90678edf25887380c9658fb12
yupix <yupi0982@outlook.jp>
2023-11-25 07:14:47 +0900

    chore: 使用していない定数を削除

--- mipac/config.py
+++ mipac/config.py
@@ -14,10 +14,6 @@ class CacheConfig:
         self.ttl: int = options.ttl
 
 
-IMisskeyDistribution = Literal["ayuskey", "m544", "areionskey", "official"]
-IMisskeyVersions = Literal[13, 12, 11]
-
-
 class ILimits(TypedDict, total=False):
     channel_name: int
     channel_description: int
