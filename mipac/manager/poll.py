4779c1bcb9a9f5d01e9da62961ce91ae05d69079
yupix <yupi0982@outlook.jp>
2023-11-29 10:49:18 +0900

    feat: ClientPollManagerを追加

--- mipac/manager/poll.py
+++ mipac/manager/poll.py
@@ -3,12 +3,25 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from mipac.abstract.manager import AbstractManager
-from mipac.actions.poll import PollActions
+from mipac.actions.poll import ClientPollActions, PollActions
 
 if TYPE_CHECKING:
     from mipac.http import HTTPClient
     from mipac.manager.client import ClientManager
 
+class ClientPollManager(AbstractManager):
+    def __init__(self, note_id: str|None=None, *, session: HTTPClient, client: ClientManager):
+        self.__note_id: str | None = note_id
+        self.__session: HTTPClient = session
+        self.__client: ClientManager = client
+    
+    @property
+    def action(self) -> ClientPollActions:
+        return ClientPollActions(
+            note_id=self.__note_id,
+            session=self.__session,
+            client=self.__client,
+        )   
 
 class PollManager(AbstractManager):
     def __init__(self, note_id: str | None = None, *, session: HTTPClient, client: ClientManager):
