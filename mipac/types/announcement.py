d4c434d0240fbb33681fa8047d2b1cc87926ea52
yupix <yupi0982@outlook.jp>
2023-12-05 14:41:14 +0900

    feat: Announcementに関するモデルと型を作り直した

--- mipac/types/announcement.py
+++ mipac/types/announcement.py
@@ -1,22 +1,35 @@
-from typing import TypedDict
+from typing import TypedDict, NotRequired
 
 
-class IAnnouncementCommon(TypedDict):
+class IAnnouncement(TypedDict):
+    id: str
+    createdAt: str
+    updatedAt: str | None
+    text: str
+    title: str
+    imageUrl: str | None
+    icon: str | None
+    display: str
+    needConfirmationToRead: bool
+    silence: bool
+    forYou: bool
+    isRead: NotRequired[bool]
+
+
+class IAnnouncementDetailed(TypedDict):
+    """管理者から見たアナウンス"""
+
     id: str
     created_at: str
     updated_at: str | None
-    text: str
     title: str
+    text: str
     image_url: str | None
-
-
-class IAnnouncement(IAnnouncementCommon):
-    """ユーザーから見たアナウンスの状態"""
-
-    is_read: bool
-
-
-class IAnnouncementSystem(IAnnouncementCommon):
-    """システムから見たアナウンスの状態"""
-
+    icon: str | None
+    display: str
+    is_active: bool
+    for_existing_users: bool
+    silence: bool
+    need_confirmation_to_read: bool
+    user_id: str | None
     reads: int
