d9ba1006be87290e165f33701ae8e3ddf61f3be0
yupix <yupi0982@outlook.jp>
2024-02-16 13:56:40 +0900

    refactor: favorite周りを再実装 progress #120

--- mipac/manager/note.py
+++ mipac/manager/note.py
@@ -43,7 +43,7 @@ class NoteManager(AbstractManager):
         self.__session: HTTPClient = session
         self.__client: ClientManager = client
         self.reaction: ReactionManager = ReactionManager(session=session, client=client)
-        self.favorite = FavoriteManager(note_id=note_id, session=session, client=client)
+        self.favorite = FavoriteManager(session=session, client=client)
         self.poll: PollManager = PollManager(note_id=note_id, session=session, client=client)
         self.__action: NoteActions = NoteActions(
             note_id=self.__note_id,
