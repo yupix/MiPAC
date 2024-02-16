56dced42d516f0a9a99fb567b6e1957e74343d36
yupix <yupi0982@outlook.jp>
2024-02-16 13:59:41 +0900

    refactor: invite周りを再実装 progress #120

--- mipac/actions/invite.py
+++ mipac/actions/invite.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, AsyncGenerator
+from typing import TYPE_CHECKING, AsyncGenerator, override
 
 from mipac.abstract.action import AbstractAction
 from mipac.http import HTTPClient, Route
@@ -13,12 +13,38 @@ if TYPE_CHECKING:
     from mipac.manager.client import ClientManager
 
 
-class ClientInviteActions(AbstractAction):
-    def __init__(self, invite_id: str | None, *, session: HTTPClient, client: ClientManager):
-        self._invite_id: str | None = invite_id
+class SharedInviteActions(AbstractAction):
+    def __init__(self, *, session: HTTPClient, client: ClientManager):
         self._session: HTTPClient = session
         self._client: ClientManager = client
 
+    async def delete(self, *, invite_id: str) -> bool:
+        """Delete an invite code.
+
+        Parameters
+        ----------
+        invite_id : str | None, optional
+            The invite code to delete, by default None
+
+        Returns
+        -------
+        bool
+            Whether the invite code was deleted.
+        """
+
+        res: bool = await self._session.request(
+            Route("POST", "/api/invite/delete"), json={"inviteId": invite_id}, auth=True
+        )
+
+        return res
+
+
+class ClientInviteActions(SharedInviteActions):
+    def __init__(self, invite_id: str, *, session: HTTPClient, client: ClientManager):
+        super().__init__(session=session, client=client)
+        self._invite_id: str = invite_id
+
+    @override
     async def delete(self, *, invite_id: str | None = None) -> bool:
         """Delete an invite code.
 
@@ -35,18 +61,12 @@ class ClientInviteActions(AbstractAction):
 
         invite_id = invite_id or self._invite_id
 
-        res: bool = await self._session.request(
-            Route("POST", "/api/invite/delete"), json={"inviteId": invite_id}, auth=True
-        )
-
-        return res
+        return await super().delete(invite_id=invite_id)
 
 
-class InviteActions(ClientInviteActions):
-    def __init__(
-        self, invite_id: str | None = None, *, session: HTTPClient, client: ClientManager
-    ):
-        super().__init__(invite_id=invite_id, session=session, client=client)
+class InviteActions(SharedInviteActions):
+    def __init__(self, *, session: HTTPClient, client: ClientManager):
+        super().__init__(session=session, client=client)
 
     async def create(self) -> InviteCode:
         """Create a new invite code.
@@ -62,24 +82,6 @@ class InviteActions(ClientInviteActions):
             Route("POST", "/api/invite/create"), auth=True
         )
         return InviteCode(raw_code, client=self._client)
-
-    async def delete(self, invite_id: str) -> bool:
-        """Delete an invite code.
-
-        Endpoint: `/api/invite/delete`
-
-        Parameters
-        ----------
-        invite_id : str
-            The invite code to delete
-
-        Returns
-        -------
-        bool
-            Whether the invite code was deleted.
-        """
-
-        return await super().delete(invite_id=invite_id)
 
     @credentials_required
     async def get_list(
