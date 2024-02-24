3ff41cac106579bb57ff9ce655fe71e1429bf9b1
yupix <yupi0982@outlook.jp>
2024-02-24 16:21:59 +0900

    feat: ClientAdminUserManagerにaccountを追加

--- mipac/manager/admins/user.py
+++ mipac/manager/admins/user.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from mipac.actions.admins.accounts import AdminAccountActions
 
 from mipac.actions.admins.user import ClientAdminUserActions, AdminUserActions
 from mipac.http import HTTPClient
@@ -11,6 +12,7 @@ if TYPE_CHECKING:
 
 class ClientAdminUserManager:
     def __init__(self, user_id: str, *, session: HTTPClient, client: ClientManager):
+        self.account: AdminAccountActions = AdminAccountActions(session=session, client=client)
         self.__action: ClientAdminUserActions = ClientAdminUserActions(
             user_id=user_id, session=session, client=client
         )
