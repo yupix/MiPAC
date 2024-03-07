60612cd0992a881b008b005f0417fb98dbb31c96
sousuke0422 <sousuke20xx@gmail.com>
2024-03-07 19:24:40 +0900

    chore: update docstring

--- mipac/actions/blocking.py
+++ mipac/actions/blocking.py
@@ -20,12 +20,40 @@ class SharedBlockingActions(AbstractAction):
         self._client: ClientManager = client
 
     async def add(self, *, user_id: str) -> UserDetailedNotMe | MeDetailed:
+        """ユーザーをブロックします
+
+        Endpoint: `/api/blocking/create`
+
+        Parameters
+        ----------
+        user_id : str
+            対象のユーザー
+
+        Returns
+        -------
+        UserDetailedNotMe | MeDetailed
+            ブロック対象のユーザー情報
+        """
         res: IUserDetailed = await self._session.request(
             Route("POST", "/api/blocking/create"), auth=True, json={"userId": user_id}, lower=True
         )
         return packed_user(res, client=self._client)
 
     async def remove(self, *, user_id: str) -> UserDetailedNotMe | MeDetailed:
+        """ユーザーのブロックを解除します
+
+        Endpoint: `/api/blocking/delete`
+
+        Parameters
+        ----------
+        user_id : str
+            対象のユーザー
+
+        Returns
+        -------
+        UserDetailedNotMe | MeDetailed
+            ブロック解除対象のユーザー情報
+        """
         res: IUserDetailed = await self._session.request(
             Route("POST", "/api/blocking/delete"), auth=True, json={"userId": user_id}, lower=True
         )
@@ -44,11 +72,39 @@ class ClientBlockingActions(SharedBlockingActions):
 
     @override
     async def add(self, *, user_id: str | None = None) -> UserDetailedNotMe | MeDetailed:
+        """ユーザーをブロックします
+
+        Endpoint: `/api/blocking/create`
+
+        Parameters
+        ----------
+        user_id : str | None, optional
+            対象のユーザー, default=None
+
+        Returns
+        -------
+        UserDetailedNotMe | MeDetailed
+            ブロック対象のユーザー情報
+        """
         user_id = user_id or self.__user_id
         return await super().add(user_id=user_id)
 
     @override
     async def remove(self, *, user_id: str | None = None) -> UserDetailedNotMe | MeDetailed:
+        """ユーザーのブロックを解除します
+
+        Endpoint: `/api/blocking/delete`
+
+        Parameters
+        ----------
+        user_id : str | None, optional
+            対象のユーザー, default=None
+
+        Returns
+        -------
+        UserDetailedNotMe | MeDetailed
+            ブロック解除対象のユーザー情報
+        """
         user_id = user_id or self.__user_id
         return await super().remove(user_id=user_id)
 
@@ -67,6 +123,24 @@ class BlockingActions(SharedBlockingActions):
         until_id: str | None = None,
         limit: int = 100,
     ) -> list[Blocking]:
+        """ブロックしているユーザーの一覧を取得します
+
+        Endpoint: `/api/blocking/list`
+
+        Parameters
+        ----------
+        since_id : str | None, optional
+            指定したIDのユーザーより後のユーザーを取得します, default=None
+        until_id : str | None, optional
+            指定したIDのユーザーより前のユーザーを取得します, default=None
+        limit : int, optional
+            一度に取得する件数, default=100
+
+        Returns
+        -------
+        list[Blocking]
+            ブロックしているユーザーの一覧
+        """
         body = {"limit": limit, "sinceId": since_id, "untilId": until_id}
         raw_blocking_list = await self._session.request(
             Route("POST", "/api/blocking/list"), json=body, auth=True
@@ -78,6 +152,24 @@ class BlockingActions(SharedBlockingActions):
     async def get_all_list(
         self, limit: int = 30, since_id: str | None = None, until_id: str | None = None
     ) -> AsyncGenerator[Blocking, None]:
+        """すべてのブロックしているユーザーの一覧を取得します
+
+        Endpoint: `/api/blocking/list`
+
+        Parameters
+        ----------
+        limit : int, optional
+            一度に取得する件数, default=30
+        since_id : str | None, optional
+            指定したIDのユーザーより前のユーザーを取得します, default=None
+        until_id : str | None, optional
+            指定したIDのユーザーより前のユーザーを取得します, default=None
+
+        Yields
+        ------
+        AsyncGenerator[Blocking, None]
+            ブロックしているユーザーの一覧
+        """
         pagination = Pagination[IBlocking](
             self._session,
             Route("POST", "/api/blocking/list"),
