da6a14df3fbfc81691b40f7d0b7b16b5cf3bb159
yupix <yupi0982@outlook.jp>
2024-06-18 17:31:47 +0900

    feat!: ClientFollowActionsの対象のIDを指定する引数を削除 #140

--- mipac/actions/follow.py
+++ mipac/actions/follow.py
@@ -90,26 +90,20 @@ class ClientFollowActions(SharedFollowActions):
         return await super().create(with_replies, user_id=user_id)
 
     @override
-    async def add(self, *, user_id: str | None = None) -> PartialUser:
-        user_id = user_id or self.__user_id
-
-        return await super().add(user_id=user_id)
+    async def add(self) -> PartialUser:
+        return await super().add(user_id=self.__user_id)
 
     @override
-    async def delete(self, *, user_id: str) -> PartialUser:
-        return await super().delete(user_id=user_id)
+    async def delete(self) -> PartialUser:
+        return await super().delete(user_id=self.__user_id)
 
     @override
-    async def remove(self, *, user_id: str | None = None) -> PartialUser:
-        user_id = user_id or self.__user_id
-
-        return await super().remove(user_id=user_id)
+    async def remove(self) -> PartialUser:
+        return await super().remove(user_id=self.__user_id)
 
     @override
-    async def invalidate(self, *, user_id: str | None = None) -> PartialUser:
-        user_id = user_id or self.__user_id
-
-        return await super().invalidate(user_id=user_id)
+    async def invalidate(self) -> PartialUser:
+        return await super().invalidate(user_id=self.__user_id)
 
 
 class FollowActions(SharedFollowActions):
